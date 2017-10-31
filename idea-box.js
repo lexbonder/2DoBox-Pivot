a12e754ff26c39c72627ac540a887d8feb4aa18f
Alex Bonder <lexbonder@gmail.com>
2017-10-31 14:24:00 -0600

    Reduce upvote and downvote funcitons to a single function

--- idea-box.js
+++ idea-box.js
@@ -11,13 +11,32 @@ $('.card-container').on('blur', 'article h2', saveTitle);
 
 $('.card-container').on('blur', 'article p', saveBody);
 
-$(document).ready(function() {
-  for (var i in localStorage) {
-    displayIdea(i);
-  }
-});
+$('#search').on('keyup', search);
+
+$('.card-container').on('click', '.upvote, .downvote', toggleQuality)
+
+$(document).ready(loadCards);
 
 // FUNCTIONS
+
+function loadCards() {
+  for (var i = 0; i < localStorage.length; i++) {
+    displayIdea(localStorage.key(i));
+  };
+};
+
+function displayIdea(id) {
+  var getArray = localStorage.getItem(id);
+  var retreivedArray = JSON.parse(getArray);
+  var object = {
+    title: retreivedArray.title,
+    body: retreivedArray.body,
+    id: retreivedArray.id,
+    qualityArray: ['none','low', 'normal', 'high', 'critical'],
+    counter: retreivedArray.counter
+  }
+  createIdea(object);
+}
 
 function pullFromStorage(id) {
   var pullCardID = localStorage.getItem(id);
@@ -55,7 +74,7 @@ function createIdea(object) {
       <p contenteditable="true">${object.body}</p>
       <button class="upvote"></button>
       <button class="downvote"></button>
-      <h3>quality: <span class="qualityValue">${object.quality}</span></h3>
+      <h3>quality: <span class="qualityValue">${object.qualityArray[object.counter]}</span></h3>
       <hr>
     </article>`)
 }
@@ -66,17 +85,7 @@ function clearInput() {
   $('#title-input').focus();
 }
 
-function displayIdea(id) {
-  var getArray = localStorage.getItem(id);
-  var retreivedArray = JSON.parse(getArray);
-  var object = {
-    title: retreivedArray.title,
-    body: retreivedArray.body,
-    id: retreivedArray.id,
-    quality: retreivedArray.quality
-  }
-  createIdea(object);
-}
+
 function deleteCard() {
   removeFromDOM(this);
   removeFromLocalStorage(this); 
@@ -123,32 +132,14 @@ function storeIdea() {
   displayIdea($id);
 }
 
-function StoreCard(title, body, id, quality) {
+function StoreCard(title, body, id, quality, counter = 2) {
   this.title = title;
   this.body = body;
   this.id = id;
   this.quality = quality;
-  this.counter = 2;
-}
+  this.counter = counter;
+};
 
-
-//-----------------------------
-//-----------------------------
-//-----------------------------
-//-----------------------------
-
-
-
-// DRY up with an || statement
-// make these one function ----------------------
-// these cancel each other out --------------
-// rename to filter?
-$('#search').on('keyup', search);
-
-// $('#search').on('keyup', function() {
-//   searchBody();
-// });
-// ------------------------------------------
 
 function pullFromStorage(id) {
   var pullCardID = localStorage.getItem(id);
@@ -168,44 +159,29 @@ function search() {
   }
 };
 
-// come back to experiment with making these shorter
-// QUALITY DOES NOT PERSIST, REVERTS TO NORMAL. ARRAY DOES PERSIST.
-
-$('.card-container').on('click', '.upvote', function() {
-  var cardQuality = $(this).closest('article');
-  var cardID = $(cardQuality).attr('id');
-  var parsedCardId = pullFromStorage(cardID)
-
-  var qualityDisplay = $(this).siblings('h3').find('.qualityValue');
-  var qualityArray = ['none','low', 'normal', 'high', 'critical'];
-  if (parsedCardId.counter === 4) {
-    $(this).attr('disabled', true);
-  } else {
+function changeQuality(event, parsedCardId, qualityDisplay, qualityArray) {
+  if (event.target.classList.contains('upvote') && parsedCardId.counter === 4) {
+    parsedCardId.counter = 4;
+  } else if (event.target.classList.contains('upvote')) {
     parsedCardId.counter++;
     qualityDisplay.text(qualityArray[parsedCardId.counter]);
-  };
-  putIntoStorage(parsedCardId);
-});
-
-
-$('.card-container').on('click', '.downvote', function() {
-  var cardQuality = $(this).closest('article');
-  var cardID = $(cardQuality).attr('id');
-  var parsedCardId = pullFromStorage(cardID)
-
-  var qualityDisplay = $(this).siblings('h3').find('.qualityValue');
-  var qualityArray = ['none','low', 'normal', 'high', 'critical'];
-  if (parsedCardId.counter === 0) {
-    $(this).attr('disabled', true);
+  } else if (event.target.classList.contains('downvote') && parsedCardId.counter === 0) {
+    parsedCardId.counter = 0;
   } else {
     parsedCardId.counter--;
     qualityDisplay.text(qualityArray[parsedCardId.counter]);
   };
-  putIntoStorage(parsedCardId);   
-});
+}
 
-
-//------------------------------
+function toggleQuality() {
+  var cardQuality = $(this).closest('article');
+  var cardID = $(cardQuality).attr('id');
+  var parsedCardId = pullFromStorage(cardID)
+  var qualityDisplay = $(this).siblings('h3').find('.qualityValue');
+  var qualityArray = ['none','low', 'normal', 'high', 'critical'];
+  changeQuality(event, parsedCardId, qualityDisplay, qualityArray)
+  putIntoStorage(parsedCardId);
+}
 
 // add two event listeners for 'enter button' functionality -------
 
