1720c300e8c81a68e49754ce87ac8ecd6314a70a
Alex Bonder <lexbonder@gmail.com>
2017-10-31 21:56:02 -0600

    Add Show more button
    Include functionality for edge cases including delete, search, add card when expanded, search while expanded, Basically, It works wonderfully

--- idea-box.js
+++ idea-box.js
@@ -13,11 +13,12 @@ $('.card-container').on('blur', 'article p', saveBody);
 
 $('#search').on('keyup', search);
 
-$('.card-container').on('click', '.upvote, .downvote', toggleQuality)
+$('.card-container').on('click', '.upvote, .downvote', toggleQuality);
 
-$(document).ready(loadFirstTen);
+$(document).ready(reloadCards);
 
-// placeholder - event listener for hold all cards function
+// placeholder - event listener for hold all cards function //
+$('.show-more-button').on('click', toggleShowMoreCards);
 
 // FUNCTIONS
 
@@ -33,13 +34,13 @@ function changeQuality(event, parsedCardId, qualityDisplay, qualityArray) {
     parsedCardId.counter--;
     qualityDisplay.text(qualityArray[parsedCardId.counter]);
   };
-}
+};
 
 function clearInput() {
   $('#description-input').val('');
   $('#title-input').val('');
   $('#title-input').focus();
-}
+};
 
 function createIdea(object) {
   $('.card-container').prepend(
@@ -51,70 +52,101 @@ function createIdea(object) {
       <button class="downvote"></button>
       <h3>quality: <span class="qualityValue">${object.qualityArray[object.counter]}</span></h3>
       <hr>
-    </article>`)
-}
+    </article>`);
+};
 
 function deleteCard() {
   removeFromDOM(this);
-  removeFromLocalStorage(this); 
-}
+  removeFromLocalStorage(this);
+  reloadCards(); 
+};
 
 function disableButton() {
   $('.save-btn').attr('disabled', true);
-}
+};
 
 function displayIdea(id) {
   var getArray = localStorage.getItem(id);
-  var retreivedArray = JSON.parse(getArray);
+  var retrievedArray = JSON.parse(getArray);
   var object = {
-    title: retreivedArray.title,
-    body: retreivedArray.body,
-    id: retreivedArray.id,
+    title: retrievedArray.title,
+    body: retrievedArray.body,
+    id: retrievedArray.id,
     qualityArray: ['none','low', 'normal', 'high', 'critical'],
-    counter: retreivedArray.counter
-  }
+    counter: retrievedArray.counter
+  };
   createIdea(object);
-}
+};
 
 function enableButton() {
   $('.save-btn').attr('disabled', false);
-}
+};
 
-// for our show more cards button
-// function loadAllCards() {
-//   for (var i = 0; i < localStorage.length; i++) {
-//     displayIdea(localStorage.key(i));
-//   };
-// };
+function toggleShowMoreCards() {
+  $('article').remove();
+  if ($('.show-more-button').text() === 'Show more...') {
+    loadAllCards();
+    $('.show-more-button').text('Show less...');
+  } else {
+    loadFirstTen();
+    $('.show-more-button').text('Show more...');
+  };
+};
+
+function reloadCards() {
+  $('article').remove();
+  if (localStorage.length > 10) {
+    revealShowMoreButton();
+    loadFirstTen();
+  } else {
+    hideShowMoreButton();
+    loadAllCards();
+  };
+};
 
 function loadFirstTen() {
-  for (var i = (localStorage.length - 11); i < (localStorage.length); i++) {
+  for (var i = (localStorage.length - 10); i < (localStorage.length); i++) {
     displayIdea(localStorage.key(i));
-  }
-}
+  };
+};
+
+function loadAllCards() {
+  for (var i = 0; i < localStorage.length; i++) {
+    displayIdea(localStorage.key(i));
+  };
+};
+
+function hideShowMoreButton() {
+  $('.show-more-button').css('display', 'none');
+};
+
+function revealShowMoreButton() {
+  $('.show-more-button').css('display', 'block');
+  $('.show-more-button').text('Show more...')
+};
 
 function pullFromStorage(id) {
   var pullCardID = localStorage.getItem(id);
   var parsedCardId = JSON.parse(pullCardID);
   return parsedCardId; 
-}
+};
 
 function saveTitle() {
   var cardID = $(this).closest('article').attr('id');
   var parsedCardId = pullFromStorage(cardID);
   parsedCardId.title = $(this).text();
   putIntoStorage(parsedCardId);
-}
+};
 
 function saveBody() {
   var cardID = $(this).closest('article').attr('id');
   var parsedCardId = pullFromStorage(cardID);
   parsedCardId.body = $(this).text();
   putIntoStorage(parsedCardId);
-}
+};
 
 function toggleSaveButton() {
-  if ($('#title-input').val() ==='') {
+  if ($('#title-input').val() === '') {
     disableButton();
   } else {
     enableButton();
@@ -124,22 +156,24 @@ function toggleSaveButton() {
 function putIntoStorage(object) {
   var stringifiedObject = JSON.stringify(object);
   localStorage.setItem(object['id'], stringifiedObject);
-}
+};
 
 function removeFromLocalStorage(event) {
   var cardToDel = $(event).closest('article');
   var cardID = $(cardToDel).attr('id');
-  localStorage.removeItem(cardID)
-}
+  localStorage.removeItem(cardID);
+};
 
 function removeFromDOM(event) {
   var cardToDel = $(event).closest('article');
   cardToDel.remove();
-}
+};
 
 function saveCard() {
   storeIdea();
   clearInput();
+  disableButton();
+  reloadCards();
   };
 
 function StoreCard(title, body, id, quality, counter = 2) {
@@ -159,7 +193,7 @@ function storeIdea() {
   var stringified = JSON.stringify(storeCard);
   localStorage.setItem($id, stringified);
   displayIdea($id);
-}
+};
 
 function pullFromStorage(id) {
   var pullCardID = localStorage.getItem(id);
@@ -168,26 +202,33 @@ function pullFromStorage(id) {
 }
 
 function search() {
- var filteredText = $(this).val().toUpperCase();
-   for (var i = 0; i < localStorage.length; i++) {
-    var parsedObject = pullFromStorage(localStorage.key(i));
-    if (parsedObject['title'].toUpperCase().includes(filteredText) || parsedObject['body'].toUpperCase().includes(filteredText)) {
-      $(`#${parsedObject['id']}`).css( "display", "block" );
-    } else {
-      $(`#${parsedObject['id']}`).css( "display", "none");
-    }
-  }
+  if ($(this).val()) {
+    $('article').remove();
+    hideShowMoreButton();
+    loadAllCards();
+    var filteredText = $(this).val().toUpperCase();
+    for (var i = 0; i < localStorage.length; i++) {
+      var parsedObject = pullFromStorage(localStorage.key(i));
+      if (parsedObject['title'].toUpperCase().includes(filteredText) || parsedObject['body'].toUpperCase().includes(filteredText)) {
+        $(`#${parsedObject['id']}`).css('display', 'block');
+      } else {
+        $(`#${parsedObject['id']}`).css('display', 'none');
+      };
+    };
+  } else {
+    reloadCards();
+  };
 };
 
 function toggleQuality() {
   var cardQuality = $(this).closest('article');
   var cardID = $(cardQuality).attr('id');
-  var parsedCardId = pullFromStorage(cardID)
+  var parsedCardId = pullFromStorage(cardID);
   var qualityDisplay = $(this).siblings('h3').find('.qualityValue');
   var qualityArray = ['none','low', 'normal', 'high', 'critical'];
-  changeQuality(event, parsedCardId, qualityDisplay, qualityArray)
+  changeQuality(event, parsedCardId, qualityDisplay, qualityArray);
   putIntoStorage(parsedCardId);
-}
+};
 
 // add two event listeners for 'enter button' functionality -------
 
